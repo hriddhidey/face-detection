c32d84a38e4ad390c826d4b592afee41be316643
Hriddhi Dey <mi0212@en-hriddhid.mynt.myntra.com>
2017-03-07 16:44:46 +0530

    Listing positions

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
     var video = document.querySelector('#camFeed');
     navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+    var pText = document.getElementById('positions');
 
     if (navigator.getUserMedia) {       
         navigator.getUserMedia({video: true}, handleVideo, videoError);
@@ -24,17 +25,26 @@
         var positions = ctracker.getCurrentPosition();
         // positions = [[x_0, y_0], [x_1,y_1], ... ]
         // do something with the positions ...
+         var positionString = "";
+          if (positions) {
+            for (var p = 0;p < 10;p++) {
+              positionString += "featurepoint "+p+" : ["+positions[p][0].toFixed(2)+","+positions[p][1].toFixed(2)+"]<br/>";
+            }
+            pText.innerHTML = positionString;
+          }
+
+        
     }
     positionLoop();
 
-    var canvasInput = document.getElementById('drawCanvas');
-    var cc = canvasInput.getContext('2d');
-    function drawLoop() {
-        requestAnimationFrame(drawLoop);
-        cc.clearRect(0, 0, canvasInput.width, canvasInput.height);
-        ctracker.draw(canvasInput);
-    }
-    drawLoop();
+    // var canvasInput = document.getElementById('drawCanvas');
+    // var cc = canvasInput.getContext('2d');
+    // function drawLoop() {
+    //     requestAnimationFrame(drawLoop);
+    //     cc.clearRect(0, 0, canvasInput.width, canvasInput.height);
+    //     ctracker.draw(canvasInput);
+    // }
+    // drawLoop();
 
 // }
 
